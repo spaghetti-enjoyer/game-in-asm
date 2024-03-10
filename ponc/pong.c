1e448f180976f76a1e53d27c0d637196f8c8993c
Leo <leonidivanovzalgo@gmail.com>
2024-03-10 18:58:17 +0100

    kinda works lol

--- ponc/pong.c
+++ ponc/pong.c
@@ -9,6 +9,9 @@
 #define BALL_SIZE 10
 #define FPS 60
 #define PADDLE_OFFSET 30
+#define BALL_BASE_SPEED_X 5
+#define BALL_BASE_SPEED_Y 2
+#define PADDLE_BASE_SPEED 5
 
 int main()
 {
@@ -19,37 +22,91 @@ int main()
     int botY = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2;
     int ballX = WINDOW_WIDTH / 2 - BALL_SIZE / 2;
     int ballY = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
-    int playerSpeed = 1;
-    int ballSpeedX = 5;
-    int ballSpeedY = 2;
+    int paddleSpeed = PADDLE_BASE_SPEED;
+    int ballSpeedX = BALL_BASE_SPEED_X;
+    int ballSpeedY = BALL_BASE_SPEED_Y;
 
     int scorePlayer = 0;
     int scoreBot = 0;
 
     while (!WindowShouldClose())
     {
+
+        // paddle mechanics
+
+        // player
+
+        if (IsKeyDown(KEY_DOWN))
+        {
+            playerY += paddleSpeed;
+        }
+        if (playerY > WINDOW_HEIGHT - PADDLE_HEIGHT)
+        {
+            playerY = WINDOW_HEIGHT - PADDLE_HEIGHT;
+        }
+        if (IsKeyDown(KEY_UP))
+        {
+            playerY -= paddleSpeed;
+        }
+        if (playerY < 0)
+        {
+            playerY = 0;
+        }
+
+        // computer
+
+        if (ballY > botY + PADDLE_HEIGHT / 2)
+        {
+            botY += paddleSpeed;
+        }
+        if (botY > WINDOW_HEIGHT - PADDLE_HEIGHT)
+        {
+            botY = WINDOW_HEIGHT - PADDLE_HEIGHT;
+        }
+        if (ballY < botY + PADDLE_HEIGHT / 2)
+        {
+            botY -= paddleSpeed;
+        }
+        if (botY < 0)
+        {
+            botY = 0;
+        }
+
+        // ball mechanics
+
         ballX += ballSpeedX;
         ballY += ballSpeedY;
 
+        // player side
+
+        if (ballX < PADDLE_OFFSET + PADDLE_WIDTH && ballY > playerY && ballY < playerY + PADDLE_HEIGHT)
+        {
+            ballX = PADDLE_OFFSET + PADDLE_WIDTH;
+            ballSpeedX *= -1;
+        }
         if (ballX < 0)
         {
-            // ballX = 0;
-            // ballSpeedX *= -1;
             scoreBot++;
             ballX = WINDOW_WIDTH / 2 - BALL_SIZE / 2;
             ballY = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
             ballSpeedX = 5;
             ballSpeedY = 2;
         }
+
+        // computer side 
+
+        if (ballX > WINDOW_WIDTH - PADDLE_OFFSET - PADDLE_WIDTH && ballY > botY && ballY < botY + PADDLE_HEIGHT)
+        {
+            ballX = WINDOW_WIDTH - PADDLE_OFFSET - PADDLE_WIDTH;
+            ballSpeedX *= -1;
+        }
         if (ballX > WINDOW_WIDTH - BALL_SIZE)
         {
-            // ballSpeedX *= -1;
-            // ballX = WINDOW_WIDTH - BALL_SIZE - 1;
             scorePlayer++;
             ballX = WINDOW_WIDTH / 2 - BALL_SIZE / 2;
             ballY = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
-            ballSpeedX = 5;
-            ballSpeedY = 2;
+            ballSpeedX = BALL_BASE_SPEED_X;
+            ballSpeedY = BALL_BASE_SPEED_Y;
         }
 
         if (ballY < 0)
@@ -65,7 +122,6 @@ int main()
             ballSpeedY *= -1;
             ballY = WINDOW_HEIGHT - BALL_SIZE - 1;
         }
-
         // printf("ball y: %i\n", ballY);
 
         BeginDrawing();
@@ -74,7 +130,7 @@ int main()
 
         char *scoreString = malloc(2 * sizeof(char));
         sprintf(scoreString, "%i", scorePlayer);
-        DrawText(scoreString, WINDOW_WIDTH / 2 - 100, 30, 32, WHITE);
+        DrawText(scoreString, WINDOW_WIDTH / 2 - 120, 30, 32, WHITE);
         sprintf(scoreString, "%i", scoreBot);
         DrawText(scoreString, WINDOW_WIDTH / 2 + 100, 30, 32, WHITE);
         free(scoreString);
